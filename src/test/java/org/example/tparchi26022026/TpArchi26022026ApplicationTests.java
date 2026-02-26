05949cb8cbdadce94f606295add4cb072de7449c
Kob0o <engerardot@gmail.com>
2026-02-26 13:48:26 +0100

    Premier Push du TP 26/02/2026

---
+++ src/test/java/org/example/tparchi26022026/TpArchi26022026ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tparchi26022026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpArchi26022026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
